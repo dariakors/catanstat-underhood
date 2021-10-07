7c7dfd1db14999bac33cd9ed813a0c5cac441c0b
Daria Korsakova <dashirak.kuznetsova@gmail.com>
2021-10-07 16:25:10 +0200

    postgres-installed

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
 from flask import Flask
+from db import db
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///postgres:postgres@localhost:5432/catangames.db'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)

5df132b4562a25fd6b0dcfe87ac9106a29776d88
Daria Korsakova <dashirak.kuznetsova@gmail.com>
2021-10-21 15:14:58 +0200

    loggers-were-added-to-game-process

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import logging
+import sys
+
 from blueprints.game import game_blueprint
 from db import db
 from flask import Flask
@@ -15,5 +18,14 @@ def create_tables():
 
 
 if __name__ == '__main__':
+    string_format = u"%(asctime)s logger:%(name)s: module:%(module)s:%(lineno)d [%(levelname)s] %(message)s"
+    formatter = logging.Formatter(string_format, "%Y-%m-%d %H:%M:%S")
+    handler = logging.StreamHandler(stream=sys.stdout)
+    handler.setFormatter(formatter)
+
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(handler)
+
     db.init_app(app)
     app.run(debug=True)
--- handlers/make_turns.py
+++ handlers/make_turns.py
@@ -1,3 +1,4 @@
+import logging
 from datetime import datetime
 from handlers import utils
 from handlers.utils import get_current_player_id
@@ -5,9 +6,14 @@ from models.game import GameModel
 from models.turn import TurnModel
 from models.turn_statuses_log import TurnStatusesModel
 
+log = logging.getLogger(__name__)
+
 
 def complete_previous_turn(game_id, cubes):
+    log.debug('Identifying current turn...')
     turn = TurnStatusesModel.find_current_turn(game_id)
+    log.debug(f'Found turn {turn.turn_id} in game {game_id} with status {turn.status}')
+
     if turn.status == 'in_progress':
         turn_with_cubes = TurnModel.find_by_id(turn.turn_id)
         turn_with_cubes.red_cube = cubes["red"]
@@ -17,30 +23,40 @@ def complete_previous_turn(game_id, cubes):
         turn.save_to_db()
         # turn_with_cubes.duration = utils.count_turn_duration(turn.turn_id)
         turn_with_cubes.save_to_db()
+        log.debug(f'Dice {cubes} are saved')
+        log.info('Previous turn is completed')
 
 
 def make_next_turn(game_id,):
     GameModel.increase_turns_number(game_id)
+    log.debug('Identifying current player...')
     current_player_id = get_current_player_id(game_id)
     turn = TurnModel(game_id, current_player_id)
     turn.save_to_db()
+    log.info('New turn is created')
     turn_status = TurnStatusesModel(turn.id, 'in_progress', start_date=datetime.utcnow())
     turn_status.save_to_db()
 
 
 def pause_turn(game_id):
+    log.debug('Identifying current turn...')
     turn = TurnStatusesModel.find_current_turn(game_id)
+    log.debug(f'Found turn {turn.turn_id} in game {game_id} with status {turn.status}')
     if turn.status == 'in_progress':
         turn.end_date = datetime.utcnow()
         turn.save_to_db()
         paused_turn = TurnStatusesModel(turn.turn_id, 'is_paused', start_date=datetime.utcnow())
         paused_turn.save_to_db()
+        log.info('The game is paused')
 
 
 def resume_turn(game_id):
+    log.debug('Identifying current turn...')
     turn = TurnStatusesModel.find_current_turn(game_id)
+    log.debug(f'Found turn {turn.turn_id} in game {game_id} with status {turn.status}')
     if turn.status == 'is_paused':
         turn.end_date = datetime.utcnow()
         turn.save_to_db()
         resumed_turn = TurnStatusesModel(turn.turn_id, 'in_progress', start_date=datetime.utcnow())
         resumed_turn.save_to_db()
+        log.info('The game is resumed')
